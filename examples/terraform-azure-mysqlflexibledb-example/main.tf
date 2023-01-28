a24a854d8d5cd946a37b96f602d6e907fcd2f902
supertrampr <piccolo.raffaele@outlook.it>
2023-01-28 02:16:36 +0100

    Update docs, variables and main in /examples.

---
+++ examples/terraform-azure-mysqlflexibledb-example/main.tf
@@ -0,0 +1,86 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# DEPLOY AN AZURE Database For MySQL - Flexible Server
+# This is an example of how to deploy an Azure Database Mysql - Flexible Server .
+# See test/terraform_azure_example_test.go for how to write automated tests for this code.
+# ---------------------------------------------------------------------------------------------------------------------
+
+terraform {
+  required_version = ">=1.0.0, <2.0.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.41.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.4.3"
+    }
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# CONFIGURE OUR AZURE CONNECTION
+# ---------------------------------------------------------------------------------------------------------------------
+
+provider "azurerm" {
+  features {}
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# DEPLOY A RESOURCE GROUP
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "azurerm_resource_group" "example" {
+  name     = "terratest-mysqlflexible-${var.postfix}"
+  location = var.location
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# DEPLOY AZURE MySQL FLEXIBLE SERVER
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Random password is used as an example to simplify the deployment and improve the security of the database.
+# This is not as a production recommendation as the password is stored in the Terraform state file.
+resource "random_password" "password" {
+  length           = 16
+  override_special = "_%@"
+  min_upper        = "1"
+  min_lower        = "1"
+  min_numeric      = "1"
+  min_special      = "1"
+}
+
+resource "azurerm_mysql_flexible_server" "example" {
+  location            = var.location
+  name                = "mysqlserver-flexible-${var.postfix}"
+  resource_group_name = azurerm_resource_group.example.name
+
+  administrator_login    = var.mysql_server_administrator_login
+  administrator_password = random_password.password.result
+
+  backup_retention_days = var.mysql_server_backup_retention_days
+  sku_name              = var.mysql_server_flexible_sku_name
+  version               = "5.7"
+  # Azure automatically deploy the instance in an Availability Zone.
+  # By providing this, when updating your configuration, you avoid error:
+  #     Error: `zone` cannot be changed independently.
+  zone = var.mysql_flexible_server_zone
+
+  storage {
+    auto_grow_enabled = false
+    size_gb           = var.mysql_server_storage_size_gb
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# DEPLOY AZURE MySQL DATABASE
+# ---------------------------------------------------------------------------------------------------------------------
+resource "azurerm_mysql_flexible_database" "example" {
+  name                = "mysqldb-flexible-${var.postfix}"
+  resource_group_name = azurerm_resource_group.example.name
+  server_name         = azurerm_mysql_flexible_server.example.name
+
+  charset   = var.mysqldb_flexible_charset
+  collation = var.mysqldb_flexible_collation
+}
---
+++ examples/terraform-azure-mysqlflexibledb-example/variables.tf
@@ -0,0 +1,73 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ENVIRONMENT VARIABLES
+# Define these secrets as environment variables
+# ---------------------------------------------------------------------------------------------------------------------
+
+# ARM_CLIENT_ID
+# ARM_CLIENT_SECRET
+# ARM_SUBSCRIPTION_ID
+# ARM_TENANT_ID
+
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# ---------------------------------------------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "location" {
+  description = "The Azure Region where the MySQL Flexible Server should exist."
+  type        = string
+  default     = "West Europe"
+}
+
+variable "mysql_server_administrator_login" {
+  description = "The Administrator login for the MySQL Flexible Server."
+  type        = string
+  default     = "mysqlflexibleadmin"
+}
+
+variable "mysql_server_backup_retention_days" {
+  description = "The backup retention days for the MySQL Flexible Server."
+  type        = number
+  default     = 7
+}
+
+variable "mysql_server_flexible_sku_name" {
+  description = "The SKU Name for the MySQL Flexible Server"
+  type        = string
+  default     = "B_Standard_B1ms"
+}
+
+variable "mysql_flexible_server_zone" {
+  description = "Specifies the Availability Zone in which this MySQL Flexible Server should be located."
+  type        = string
+  default     = "1"
+}
+
+variable "mysql_server_storage_size_gb" {
+  description = "The max storage allowed for the MySQL Flexible Server."
+  type        = string
+  default     = "20"
+}
+
+variable "mysqldb_flexible_charset" {
+  description = "Specifies the Charset for the MySQL Database."
+  type        = string
+  default     = "utf8"
+}
+
+variable "mysqldb_flexible_collation" {
+  description = "Specifies the Collation for the MySQL Database"
+  type        = string
+  default     = "utf8_unicode_ci"
+}
+
+variable "postfix" {
+  description = "A postfix string to centrally mitigate resource name collisions."
+  type        = string
+  default     = "example"
+}
